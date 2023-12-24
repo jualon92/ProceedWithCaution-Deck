5ed0f380c5c7e9033ad9ba40a47a8a2741a7cc33
Juan Ignacio Londra <londrajuan@gmail.com>
2023-12-24 07:35:29 -0300

    fix cards mobile layout

--- src/app/tab1/card-db.ts
+++ src/app/tab1/card-db.ts
@@ -2,20 +2,20 @@ import { Card } from "./entity"
 
 const card1:Card = {
   title: 'Card 1',
-  description: 'This is card 1',
+  description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam eu tortor sit amet risus elementum pharetra vel vel nisl. Sed ut mauris at tortor semper semper. Curabitur ac luctus justo. Integer consectetur dui id dictum sodales. Cras dignissim finibus nunc consequat cursus. Nulla quis nulla porttitor lorem varius laoreet.',
    subtitle: 'This is subtitle1',
   image: '../../assets/images/card-media.png'
 }
 const card2 = {
   title: 'Card 2',
-  description: 'This is card 2',
-  subtitle: 'This is subtitle2',
+  description: ' orci nisi laoreet velit, at pharetra urna nu nia tortor eget felis sagittis efficitur. Etiam congue orci purus ',
+  subtitle: 'nc eget neque. Maecenas laci , fermentum tempus mauris tincidunt id. Pellentesque dapibus leo eu aliquam aliquet. Proin dictum velit vitae elit porttitor vehicula. Phasellus nec finibus augue. Fusce vestibulum felis tristique porta gravida. Curabitur gravida ut odio ac eleifend. Ut eu nulla mauris.',
   image: '../../assets/images/card-media.png'
 }
 
 const card3 = {
   title: 'Card 3',
-  description: 'This is card 3',
+  description: 'ris ullamcorper, ultricies fringilla libero eleifend. Integer vel ligula luctus, euismod urna sit amet, ultrices tellus',
   subtitle: 'This is subtitle3',
   image: '../../assets/images/card-media.png'
 }
@@ -23,7 +23,7 @@ const card3 = {
 
 export const notEnoughCardsCard = {
   title: 'Deck is empty!',
-  description: 'not enough cards in the deck',
+  description: 'Maecenas venenatis suscipit orci a porttitor. Phasellus posuere turpis ex, ut tincidunt dolor sagittis a. Nulla facilisi. Nunc ac consequat sapien, at faucibus justo. Nulla facilisis, nunc vitae iaculis ultricies, ligula lacus pulvinar metus, vel euismod felis dui ac ipsum. Ut tempus hendrerit eros, ut malesuada nulla varius nec. ',
   subtitle: ':(',
   image: 'https://picsum.photos/200/300'
 }
--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -5,6 +5,7 @@ import { shuffle } from 'lodash-es';
 import { first } from 'rxjs';
 @Component({
   selector: 'app-tab1',
+
   templateUrl: 'tab1.page.html',
   styleUrls: ['tab1.page.scss']
 })
