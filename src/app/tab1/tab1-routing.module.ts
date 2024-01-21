d832fe4abe04b0d30a20f59f65ce45c3eaf74778
Juan Ignacio Londra <londrajuan@gmail.com>
2024-01-21 01:53:56 -0300

    add play tab routing

--- src/app/tab1/tab1-routing.module.ts
+++ src/app/tab1/tab1-routing.module.ts
@@ -1,12 +1,23 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { Tab1Page } from './tab1.page';
+import { BeginPlayingComponent } from './begin-playing/begin-playing.component';
+import { DrawingCardsComponent } from './drawing-cards/drawing-cards.component';
+import { NoCardLeftComponent } from './no-card-left/no-card-left.component';
 
 const routes: Routes = [
   {
     path: '',
     component: Tab1Page,
-  }
+  },
+  {
+    path: 'draw-cards',
+    component: DrawingCardsComponent,
+  },
+  {
+    path: 'end-game',
+    component: NoCardLeftComponent,
+  },
 ];
 
 @NgModule({
--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -1,67 +1,23 @@
-import { ChangeDetectorRef, Component, computed, signal } from '@angular/core';
-import { deck, notEnoughCardsCard } from './card-db';
+ import { deck, notEnoughCardsCard } from './card-db';
 import { Card } from './entity';
 import { shuffle } from 'lodash-es';
 import { first } from 'rxjs';
+import { Component, ChangeDetectionStrategy, signal } from '@angular/core';
 @Component({
   selector: 'app-tab1',
-
   templateUrl: 'tab1.page.html',
   styleUrls: ['tab1.page.scss'],
+  changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class Tab1Page {
   isInBeginPlayingScreen = signal<boolean>(true);
-  initialDeck = shuffle(deck);
-  readonly playerDeck  = signal<Card[]>(this.initialDeck);
-  readonly gameEnded =  signal<boolean>(false);
-  //actual card is the card at the top of the deck
-  readonly actualCard  = computed( ()  => this.playerDeck()[0]);
-/*   readonly cardMessage = computed( () => this.actualCard().title); */
-  discardPile: Card[] = [];
-  skippedCards: Card[]= []
-  completedCards: Card[] = [];
-  completedCardMessage: string = "";
-  skippedCardMessage: string = "";
-  lastCardName: string = "";
-  cardToggle = true;
+  gameEnded =  signal<boolean>(false);
 
-  constructor(public cd: ChangeDetectorRef) {
+
+  constructor() {
 
   }
 
- 
-  drawNextCard(){
-    this.lastCardName = this.actualCard().title;
-    //add the card to card history
-    this.discardPile.push(this.actualCard());
-    //remove card from the deck
-    this.playerDeck.update( card => card.filter( c => c.title !== this.actualCard().title));
-    this.toggleCardAnimation();
-  }
-
-
-  toggleCardAnimation():void{
-    //TODO: dont trigger additional detection cycles
-    this.cardToggle = false;
-    this.cd.detectChanges();
-    this.cardToggle = true;
-    this.cd.detectChanges();
-
-  }
-  addToCompletePile(){
-    this.completedCards.push(this.actualCard());
-  }
-
-  addToSkippedPile(){
-      this.skippedCards.push(this.actualCard());
-  }
-
-  shuffleDeck(){
-   //redo the deck
-    this.playerDeck.set(shuffle(deck));
-    this.discardPile = [];
-    this.gameEnded.set(false);
-  }
 
 
 }
