e8eb0847c5a29278f553e1331be6c3d5220e8ebb
Juan Ignacio Londra <londrajuan@gmail.com>
2024-01-29 02:53:52 -0300

    add medals objects

--- src/app/tab1/entity.ts
+++ src/app/tab1/entity.ts
@@ -8,14 +8,27 @@ export interface Card {
 }
 
 export const Difficulties = {
-  Hard : { color: "danger", message: "Hard as a rock"},
-  Medium : { color: "warning", message: "Not too hard, not too easy"},
-  Easy : { color: "primary", message: "Easy peasy lemon squeezy"},
+  Hard : { color: "danger", message: "Hard as a rock", points: 3},
+  Medium : { color: "warning", message: "Not too hard, not too easy", points: 2},
+  Easy : { color: "primary", message: "Easy peasy lemon squeezy", points: 1},
+}
+
+export const Medals = {
+  Bronze : { color: "warning", message: "Bronze", image : "../../assets/images/bronze-medal.png"},
+  Silver : { color: "secondary", message: "Silver", image: "../../assets/images/silver-medal.png"},
+  Gold : { color: "warning", message: "Gold", image: "../../assets/images/gold-medal.png"},
+}
+
+export interface Medal {
+  color: string,
+  message: string,
+  image: string,
 }
 
 export interface Difficulty{
   color: string,
   message: string,
+  points: number,
 }
 
 export interface Deck {
