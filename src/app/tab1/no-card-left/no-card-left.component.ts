b0f02fd8e0c2a1cf392d6ff605c15d6cfc3b6f3e
Juan Ignacio Londra <londrajuan@gmail.com>
2024-03-10 03:08:15 -0300

    save points on storage after game ends

--- src/app/tab1/no-card-left/no-card-left.component.ts
+++ src/app/tab1/no-card-left/no-card-left.component.ts
@@ -1,7 +1,8 @@
 import { Component, EventEmitter, OnInit, Output } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
 import { IonicModule } from '@ionic/angular';
-import { Medal, Medals } from '../entity';
+import { Medal, Medals, PointsByDate } from '../entity';
+import { StorageService } from '../../storage-service';
 @Component({
   selector: 'app-no-card-left',
   templateUrl: './no-card-left.component.html',
@@ -11,13 +12,15 @@ export class NoCardLeftComponent implements OnInit {
   @Output() shuffleDeck: EventEmitter<any> = new EventEmitter();
   medal : Medal = { color: "warning", message: "Bronze", image : "./assets/images/bronze.png"}
 
-  constructor(private router: Router, private route : ActivatedRoute) { }
+  constructor(private router: Router, private route : ActivatedRoute, private storageService: StorageService) { }
 
   ngOnInit() {
     // get query params from url
      this.route.queryParams.subscribe(params => { 
           const points = params['points'];
-          this.medal = this.getMedalsByPoints(points);
+          const pointsByDate = {points: points, date: new Date()}
+          this.storageService.savePoints(pointsByDate)
+          this.medal = this.getMedalsByPoints(points);       
      })
   }
 
